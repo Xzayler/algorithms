8dc4bd410243cdc0372fef5efa4c61161e950d2d
Xzayler <csak.mityu@gmail.com>
2024-09-11 14:30:17 +0200

    chore: Create Container component
    Container wraps sections of the page to normalize sizes and ensuring a sufficient padding

---
+++ src/components/shared/Container.tsx
@@ -0,0 +1,9 @@
+import { JSXElement } from 'solid-js';
+
+export default function Container(props: { children: JSXElement }) {
+  return (
+    <div class="max-w-screen-md w-full xs:w-11/12 sm:w-10/12 md:w-full min-w-min px-4 mx-auto max-h-full">
+      {props.children}
+    </div>
+  );
+}
