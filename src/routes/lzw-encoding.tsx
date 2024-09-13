a9a07af402f9d38ba8c97a8b4ba91bae899d818e
Xzayler <csak.mityu@gmail.com>
2024-09-13 12:09:16 +0200

    feat: Add decoding feature

--- src/routes/lzw-encoding.tsx
+++ src/routes/lzw-encoding.tsx
@@ -1,24 +1,57 @@
 import { createSignal } from 'solid-js';
 
-import { encode, EncodingData } from '~/algos/LZWEncoder';
+import { encode, decode, EncodingData, DecodingData } from '~/algos/LZWEncoder';
 
 import Container from '~/components/shared/Container';
 import EncodingTable from '~/components/encoding/EncodingTable';
 import { TextField } from '@kobalte/core/text-field';
 import { Button } from '@kobalte/core/button';
 import { Switch } from '@kobalte/core/switch';
+import { delimiter } from 'vinxi/dist/types/lib/path';
 
 export default function LZWEndoding() {
   const [tableData, setTableData] = createSignal<EncodingData>({});
   const [input, setInput] = createSignal<string>('');
   const [output, setOutput] = createSignal<string>('');
+  const [inputError, setInputError] = createSignal<string>('');
 
   // false = encoding, true = decoding
   const [mode, setMode] = createSignal<boolean>(false);
 
+  function parseDecodingInput(input: string): [string, DecodingData] {
+    const separator = ' }\n';
+    const indexOfFirstSeparator = input.indexOf(separator);
+    if (indexOfFirstSeparator === -1) {
+      setInputError('Invalid input format');
+      return ['', []];
+    }
+    const codeStr: string = input.substring(2, indexOfFirstSeparator);
+    const codes: string[] = codeStr.split(', ');
+    console.log(codes);
+    const decodingData: string[] = [];
+    // const decodingData: string[] = new Array(codes.length);
+    for (const val of codes) {
+      const [str, index] = val.split(': ', 2);
+      decodingData.splice(Number(index) - 1, 0, str);
+    }
+
+    const inputStr = input.substring(indexOfFirstSeparator + separator.length);
+    console.log(inputStr);
+    console.log(decodingData);
+
+    return [inputStr, decodingData];
+  }
+
   function handleSubmit() {
     if (mode()) {
-      setOutput('decoded message');
+      try {
+        const [inputStr, inputCodes] = parseDecodingInput(input());
+        const { decodedStr, table } = decode(inputStr, inputCodes);
+        setTableData(table);
+        setOutput(decodedStr);
+      } catch (e) {
+        console.log((e as Error).message);
+      }
     } else {
       const { codedStr, table } = encode(input());
       setTableData(table);
@@ -42,15 +75,21 @@ export default function LZWEndoding() {
                 onChange={setInput}
                 class="flex flex-col h-full w-full"
               >
-                <TextField.Label>Insert text to Encode</TextField.Label>
-                <TextField.TextArea class="grow border border-ui rounded-md text-foreground bg-background resize-none" />
+                <TextField.Label>
+                  {'Insert text to ' + (mode() ? 'Decode' : 'Encode')}
+                </TextField.Label>
+                <TextField.TextArea class="p-1 grow border border-ui rounded-md text-foreground bg-background resize-none scrollbar-thin scrollbar-corner-transparent scrollbar-thumb-ui scrollbar-track-transparent" />
               </TextField>
             </div>
 
-            <div class="flex items-center gap-2">
+            <div class="flex items-center gap-2 flex-wrap">
               <Switch
                 class="flex flex-col items-center peer"
-                onChange={setMode}
+                onChange={(isChecked: boolean) => {
+                  setMode(isChecked);
+                  setInput(output());
+                  setOutput('');
+                }}
               >
                 <Switch.Label>Encode/Decode</Switch.Label>
                 <Switch.Input />
@@ -60,7 +99,7 @@ export default function LZWEndoding() {
               </Switch>
               <Button
                 onClick={handleSubmit}
-                class=" font-bold text-lg py-2 px-3 rounded-xl transition-opacity bg-sky-700 peer-data-[checked]:bg-amber-700 hover:!bg-opacity-70 "
+                class="w-24 font-bold text-lg py-2 px-3 rounded-xl transition-opacity bg-sky-700 peer-data-[checked]:bg-amber-700 hover:!bg-opacity-70 "
               >
                 {mode() ? 'Decode' : 'Encode'}
               </Button>
@@ -70,7 +109,7 @@ export default function LZWEndoding() {
           <div class="min-w-0">
             <TextField value={output()} class="flex flex-col h-full">
               <TextField.Label>Output</TextField.Label>
-              <TextField.TextArea class="grow border border-ui rounded-md text-foreground bg-background resize-none" />
+              <TextField.TextArea class="p-1 grow border border-ui rounded-md text-foreground bg-background resize-none scrollbar-thin scrollbar-corner-transparent scrollbar-thumb-ui scrollbar-track-transparent" />
             </TextField>
           </div>
         </div>
